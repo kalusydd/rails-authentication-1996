429f37b63b804993a7815e5cba6ab0773cff5535
Kat Syddall <kat.syddall@gmail.com>
2025-05-26 10:36:31 +0200

    initial app setup with devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
+  end
+
+  def profile
+    @first_name = current_user.email.split('@').first.capitalize
   end
 end
